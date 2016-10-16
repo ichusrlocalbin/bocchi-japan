6fc36b3ea7032511fb95751a6702d12b62924614
unagoccia <una.goccia.della.luna@gmail.com>
2016-10-17 00:11:38 +0900

    入力したIDでAPIを実行するように修正
    ※その他：レイアウトを少し変更

--- front/src/js/components/MatchingList.jsx
+++ front/src/js/components/MatchingList.jsx
@@ -7,11 +7,13 @@ class MatchingList extends Component {
         super(props)
 
         this.state = {
-            data: null
+            data: null,
+            myid: ""
         }
     }
 
     getList(twitterId) {
+        console.log(twitterId);
         return new Promise((resolve,reject) => {
             // request.get("https://bocchi-japan.herokuapp.com/tweet_demo?id=" + twitterId)
             request.get("https://bocchi-japan.herokuapp.com/tweet?id=" + twitterId)
@@ -27,46 +29,57 @@ class MatchingList extends Component {
         });
     }
 
+    onMyidChange(e) {
+        this.setState({myid: e.target.value});
+    }
+
     onGetButtonClick() {
-        this.getList("83496671")
-        .then(
-            (response) => {
-                console.log(response);
-                this.setState({data: response});
-            }
-        ).catch(
-            (err) => { console.error(err); }
-        );
+        if(this.state.myid) {
+            // this.getList("83496671")
+            this.getList(this.state.myid)
+            .then(
+                (response) => {
+                    console.log(Object.keys(response).length);
+                    if(Object.keys(response).length > 0) {
+                        this.setState({data: response});
+                    } else {
+                        alert("マッチングしませんでした。");
+                    }
+                }
+            ).catch(
+                (err) => { console.error(err); }
+            );
+        } else {
+            alert("ツイッターIDを入力してください。");
+        }
     }
 
     render() {
         // console.log(this.state.data.user.id);
         let youid = "";
         let place = "取得ボタンをおしてね！";
+        let match = "Find me!!";
         if(this.state.data) {
             youid = this.state.data.user.screen_name;
             place = this.state.data.place.country + " " + this.state.data.place.name + "now!!";
+            match = "MATCH!";
             // console.log(this.state.data);
         }
-
         return (
             <div className="wrapper">
                 <div className="logo">
-                     <p>Bocchi Japan</p>
+                    <p className="logo-text">Bocchi Japan</p>
+                    <p><input className="myid" type="text" placeholder="input twitter id." value={this.state.myid} onChange={this.onMyidChange.bind(this)}/></p>
+                    <p><Button bsStyle="warning" onClick={this.onGetButtonClick.bind(this)}>Matiching!!</Button></p>
                 </div>
                 <div className="match_area">
             		<p className="place"><span className="text2">{place}</span></p>
                 </div>
-                <span className="match"><p className="text">MATCH!</p></span>
+                <span className="match"><p className="text3">{youid}</p><p className="text">{match}</p></span>
                 <p className="img">
                     <img className="pumpkin" src="src/img/pumpkin-human.png" alt="かぼちゃマスクをかぶった品源人間"/>
                 </p>
-                <div className="match_area_id">
-                    <input className="myid" type="text" placeholder="input twitter id."/>
-                    <span className="youid">{youid}</span>
-                </div>
                 <div className="matching-view-area">
-                    <Button onClick={this.onGetButtonClick.bind(this)}>取得</Button>
                 </div>
             </div>
 
